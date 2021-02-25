9be4feb10026485e184b35ac33bf8168ae58ac24
Dominik Šebák <dominiksebak@gmail.com>
2021-02-25 22:57:51 +0100

    no message

--- payout_test/src/constants.cs
+++ payout_test/src/constants.cs
@@ -2,8 +2,8 @@ namespace Payout.Lib
 {
     public static class Constants
     {
-        public const string KEY = "00706602-bcb6-48e1-939b-b3f02626f2e0";
-        public const string SECRET = "zXqWzZMCaa7F7GiT64Vnd7Yy0CoqaaHpUySxHfSfE9AF8QBtToo0WYO5y-BTwl0L";
+        public const string KEY = "";
+        public const string SECRET = "";
 
         public const string HOST = "sandbox.payout.one";
     }
